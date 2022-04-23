28aab5dd7869c5fe919a3c5de2a2366fde21b47e
Jeffrey Muriithi <jefahk@gmail.com>
2022-04-23 23:39:05 +0300

    Third commit

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,12 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable comma-dangle */
+/* eslint-disable linebreak-style */
+/* eslint-disable global-require */
+/* eslint-disable linebreak-style */
+/* eslint-disable eol-last */
+/* eslint-disable indent */
+/* eslint-disable linebreak-style */
+
 module.exports = {
     plugins: [require('tailwindcss'), require('autoprefixer')]
   };
